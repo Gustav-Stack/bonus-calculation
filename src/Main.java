ac0d6f426de58a6b6ece024a6739b0304779d525
Gustav-Stack <lucenagustavo404@gmail.com>
2026-05-04 17:48:21 -0300

    feature: adding polimorfism to 'funcionarios'

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-    Funcionario funcionario = new Funcionario("Carlos", 3000);
-    funcionario.calcularBonus();
+
+        List<Funcionario> funcionarios = new ArrayList<>();
+        funcionarios.add(new Funcionario("Carlos", 3000));
+        funcionarios.add(new Engenheiro("Cristiano", 4000));
+        funcionarios.add(new Vendedor("Julia", 2500, 25000));
+        funcionarios.add(new Gerente("Augusto", 3000));
 
 
-        Engenheiro engenheiro = new Engenheiro("Cristiano", 4000);
-        engenheiro.calcularBonus();
 
-        Vendedor vendedor = new Vendedor("Julia", 2500, 25000);
-        vendedor.calcularBonus();
-
-        Gerente gerente = new Gerente("Augusto", 3000);
-        gerente.calcularBonus();
+        for(Funcionario f : funcionarios){
+            f.calcularBonus();
+        }
 
     }
 
